61b62c78b119193a3f5b649a345c6fce63f7f810
geek10010 <36638017+geek10010@users.noreply.github.com>
2018-02-28 00:02:04 +0300

    src/index.js zeros function 1 version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function getZerosCount(number) {
   let countFives = 0;
   let currentNum;
-  //
-  //console.log("number: "+ number);
+  
+  /*
   for (let i=5; i<=number; i = i + 5) {
     currentNum = i;
     while (currentNum !== 1 && currentNum % 5 === 0) {
@@ -12,26 +12,13 @@ module.exports = function getZerosCount(number) {
       currentNum = Math.floor(currentNum / 5);
     }
   }
-  return countFives;
-}
+  */
 
-
-/*
-module.exports = function getZerosCount(number) {
-  let countFives = 0;
-  let currentNum;
-  //
-  //console.log("number: "+ number);
-  for (let i=5; i<=number; i = i + 5) {
-    currentNum = i;
-    while (currentNum !== 1 && currentNum % 5 === 0) {
-      //
-      //console.log("currentNum5: " + currentNum);
-      countFives++;
-      currentNum = Math.floor(currentNum / 5);
-    }
+  // mad idea - go faster! :)
+  let powerFive = Math.floor(Math.log(number) / Math.log(5));
+  for (let i=1; i<=powerFive; i++) {
+    countFives += Math.floor(number / 5 ** i);
   }
+  
   return countFives;
 }
-
-*/
