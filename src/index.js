bb821d92d592cf1d2de4378be3c892fc9c4b60d0
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-12 10:16:16 +0300

    src/index.js linting of function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function getZerosCount(number) {
   let countFives = 0;
-  let powerFive = Math.floor(Math.log(number) / Math.log(5));
-  
-  for (let i=1; i<=powerFive; i++) {
-    countFives += Math.floor(number / 5 ** i);
+  const powerFive = Math.floor(Math.log(number) / Math.log(5));
+
+  for (let i = 1; i <= powerFive; i++) {
+    countFives += Math.floor(number / (5 ** i));
   }
-  
+
   return countFives;
-}
+};
